d1b881b41b26ce9c04b7581a98c855149a110319
RLMWLKR <chasep1010@comcast.net>
2025-11-06 10:24:01 -0800

    Boss (In progress)

---
+++ Assets/Scripts/Spike.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*
+ * Chase Phillips
+ * 11/5/25
+ * Handles the spikes movement and timing
+ */
+
+
+public class Spike : MonoBehaviour
+{
+    public GameObject SpikePrefab;
+    public Surprise alive;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        if (alive.Boss)
+        {
+
+        }
+
+    }
+}
---
+++ Assets/Scripts/Surprise.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Chase Phillips
+ * 11/5/25
+ * Handles the effects/attacks of the boss and the boss room
+ */
+
+public class Surprise : MonoBehaviour
+{
+
+    public bool Boss; //Detects if boss is still alive, if so run code.
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Boss = true;
+    }
+
+
+
+
+
+
+}
