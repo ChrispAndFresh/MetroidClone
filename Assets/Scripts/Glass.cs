ddbefe38a438158b71297c1f3cb5e225e6fdf237
ChrispAndFresh <chrispimentel888@gmail.com>
2025-11-06 12:12:42 -0800

    breakable glass walls

--- Assets/Scripts/Glass.cs
+++ Assets/Scripts/Glass.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * Chris Pimentel
+ * 11/6/25
+ * Allows for destructable walls if player has missiles
+ */
+
 public class Glass : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //How much damage the player needs to do to destroy the wall
+    public int health = 3;
 
-    // Update is called once per frame
-    void Update()
+    //If the player hits glass with a strong enough bullet, destroy glass
+    public void Destroy(int damage)
     {
-        
+        if (damage >= health)
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
